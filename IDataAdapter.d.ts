c41412c3cbc4672e017e91011e17ec51ec6f8090
Alexandru Ciuca <alexandru.ciuca@consensys.net>
2019-07-26 15:14:03 +0300

    Fix type mismatch in createWatcher signature

--- IDataAdapter.d.ts
+++ IDataAdapter.d.ts
@@ -3,5 +3,5 @@ import { CancellationToken } from "@puzzl/core/lib/async/cancellation";
 export interface IDataAdapter<TContext, TData> {
     contextType: Record<keyof TContext, "number" | "string">;
     load(context: TContext, cancellationToken: CancellationToken): Promise<TData | undefined>;
-    createWatcher?(context: TContext, lastData: TData): IDataWatcher | IDataWatcher[];
+    createWatcher?(context: TContext, lastData: TData | undefined): IDataWatcher | IDataWatcher[];
 }
