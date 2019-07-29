e35817231b5a3b4375f6ef9bf34b1d9dbad078ec
Alexandru Ciuca <alexandru.ciuca@consensys.net>
2019-07-29 16:05:38 +0300

    Add alias for data adapter configs (ref variant)

--- IDataAdapterConfig.d.ts
+++ IDataAdapterConfig.d.ts
@@ -8,10 +8,12 @@ export interface IDataAdapterRefConfig {
      * and the data will be 'undefined'. Use this for non-critical adapters or when managing loading indicators manually
      */
     optional?: boolean;
+    /** An alias for the adapter URI, used for getting the adapter result easier from asyncData. */
+    alias?: string;
 }
 export interface IDataAdapterInlineConfig<TContext> {
     def: IDataAdapter<TContext, unknown>;
-    /** Key used for asyncData mapping */
+    /** An alias for the adapter URI, used for getting the adapter result easier from asyncData. */
     alias: string;
     /**
      * If true, the module will be rendered before data from this adapter was fully loaded,
